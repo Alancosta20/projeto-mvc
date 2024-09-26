7a0730ea0ec73bcb160c7116cd4377f05a2e10ae
Zuadinho <alancostacs@hotmail.com>
2024-09-26 19:29:01 -0300

    Edit e Update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show]
+  before_action :set_product, only: [:show, :edit]
 
   def index
     @products = Product.all
@@ -13,11 +13,21 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-      if @product.save
-        redirect_to products_url
-      else
-        render :new, status: :unprocessable_entity
-      end
+    if @product.save
+      redirect_to products_url
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @product.update(product_params)
+      redirect_to products_url(@product)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
     
   private
