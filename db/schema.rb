e4cb147951d0a2f996a2648029fb1ee750d358ac
Zuadinho <alancostacs@hotmail.com>
2024-10-02 16:01:31 -0300

    Criação das models: author account e books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_16_224656) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_02_173104) do
+  create_table "accounts", force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_accounts_on_author_id"
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.decimal "price"
@@ -19,4 +40,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_16_224656) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "accounts", "authors"
+  add_foreign_key "books", "authors"
 end
