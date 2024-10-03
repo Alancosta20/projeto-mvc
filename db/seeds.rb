646fc9eec112e2bd67793bacd9af902f3c726aca
Zuadinho <alancostacs@hotmail.com>
2024-10-03 18:07:42 -0300

    through

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+medico = Doctor.create(name: "Estranho 2")
+medico2 = Doctor.create(name: "outro medico")
+
+paciente = Patient.create(name: "kallebe")
+paciente2 = Patient.create(name: "Alan")
+
+Appointment.create(doctor: medico, patient: paciente, date: Date.today)
+Appointment.create(doctor: medico2, patient: paciente2, date: Date.today)
+Appointment.create(doctor: medico, patient: paciente, date: Date.today)
+Appointment.create(doctor: medico2, patient: paciente2, date: Date.today)
