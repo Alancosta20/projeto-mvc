f0c0ce3f1570b2dbaf63180ec33493f74e826e7a
Zuadinho <alancostacs@hotmail.com>
2024-10-09 16:30:45 -0300

    Associação "has_and_belongs_to_many"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_07_183610) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_09_184818) do
   create_table "account_histories", force: :cascade do |t|
     t.date "date"
     t.integer "account_supplier_id", null: false
@@ -78,7 +78,26 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_07_183610) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students_teachers", force: :cascade do |t|
+    t.integer "student_id"
+    t.integer "teacher_id"
+    t.index ["student_id"], name: "index_students_teachers_on_student_id"
+    t.index ["teacher_id"], name: "index_students_teachers_on_teacher_id"
+  end
+
   create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teachers", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,19 @@
-medico = Doctor.create(name: "Estranho 2")
-medico2 = Doctor.create(name: "outro medico")
+#medico = Doctor.create(name: "Estranho 2")
+#medico2 = Doctor.create(name: "outro medico")
 
-paciente = Patient.create(name: "kallebe")
-paciente2 = Patient.create(name: "Alan")
+#paciente = Patient.create(name: "kallebe")
+#paciente2 = Patient.create(name: "Alan")
 
-Appointment.create(doctor: medico, patient: paciente, date: Date.today)
-Appointment.create(doctor: medico2, patient: paciente2, date: Date.today)
-Appointment.create(doctor: medico, patient: paciente, date: Date.today)
-Appointment.create(doctor: medico2, patient: paciente2, date: Date.today)
+#Appointment.create(doctor: medico, patient: paciente, date: Date.today)
+#Appointment.create(doctor: medico2, patient: paciente2, date: Date.today)
+#Appointment.create(doctor: medico, patient: paciente, date: Date.today)
+#Appointment.create(doctor: medico2, patient: paciente2, date: Date.today)
 
-s1 = Supplier.create(name: "Diego")
+#s1 = Supplier.create(name: "Diego")
 
-a1 = AccountSupplier.create(number: 144, supplier: s1)
+#a1 = AccountSupplier.create(number: 144, supplier: s1)
 
-AccountHistory.create(date: Date.today, account_supplier: a1)
+#AccountHistory.create(date: Date.today, account_supplier: a1)
+
+s1 = Student.create(name: "noob")
+t1 = Teacher.create(name: "Diego")
